61484ce039f078d3a0c457c2342a2943bc87c1bb
LIJIKN <102722441+LIJIKN@users.noreply.github.com>
2022-11-06 23:04:44 +0530

    new

--- frontend/src/app/username/username.component.ts
+++ frontend/src/app/username/username.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-username',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit {
+username="";
+User_form  = new FormGroup({
+  nickname : new FormControl(this.username,[Validators.required,]),
 
-  constructor() { }
+})
+  constructor(private router:Router,private service:AuthService) { }
 
   ngOnInit(): void {
+  }
+  get nickname(){return this.User_form.controls.nickname;}
+  onSubmitNAME(values:any){
+    
+
   }
 
 }
